8e91ac47168d6a3ee4c2137847f98a6d8d73f2a5
Aditya Kumar singh <ksingh10adi@gmail.com>
2026-02-18 09:09:38 +0530

    updated routing path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://hackmatch-v9nv.onrender.com/api"
 });
 
 // 🔥 Attach token automatically
